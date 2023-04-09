72de0c55833120b429bdad052ac111cdc1d37dc7
Michael Trip <michael@alcatrash.net>
2023-04-09 20:40:31 +0200

    changed mirror

--- manjaro2arch.sh
+++ manjaro2arch.sh
@@ -27,7 +27,7 @@ Include = /etc/pacman.d/mirrorlist
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 EOF
-[ -z "$SERVER" ] && SERVER='https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch'
+[ -z "$SERVER" ] && SERVER='http://ftp.snt.utwente.nl/pub/linux/manjaro/stable/$repo/$arch'
 cat <<EOF >/etc/pacman.d/mirrorlist
 Server = $SERVER
 EOF
