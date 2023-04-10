ddbcf31d40bf916bd83edcf71c1ee720cd5305a4
Michael Trip <michael@alcatrash.net>
2023-04-10 19:24:10 +0200

    testing with script

--- manjaro2arch.sh
+++ manjaro2arch.sh
@@ -37,4 +37,35 @@ pacman -Syyuu --noconfirm filesystem pacman bash linux breeze-grub breeze-gtk ls
 pacman -Rdd   --noconfirm $(pacman -Qq | grep -E 'manjaro|breath')
 cp /usr/share/grub/themes/breeze /boot/grub/themes/
 sed -i 's/^GRUB_THEME.*$/GRUB_THEME="/boot/grub/themes/breeze/theme.txt"/g' /etc/default/grub && grub-mkconfig -o /boot/grub/grub.cfg
+
+cat <<EOF >~/reinstall-packages.sh
+
+#!/bin/bash
+
+if [ `id -u` -eq 0 ];then
+  echo "Reinstalling all packages"
+else
+  echo "Please run with root permission"
+  exit 1
+fi
+
+# Create a list of explicitly installed packages
+pacman -Qeq > package-list.txt
+
+# Create a list of all installed packages, including dependencies
+pacman -Qeq | sed 's/\(^.*\)/\1 \1/g' | xargs pacman -Qq | sort -u > all-package-list.txt
+
+# Create a list of packages that are not dependencies
+comm -13 <(pacman -Qtdq | sort) <(pacman -Qqg base base-devel linux linux-firmware | sort -u) > non-dependency-list.txt
+
+# Combine the lists of explicitly installed packages and non-dependency packages
+cat package-list.txt non-dependency-list.txt | sort -u > reinstall-list.txt
+
+# Reinstall all packages from the combined list
+sudo pacman -S --force --noconfirm - < reinstall-list.txt
+EOF
+chmod +x ~/reinstall-packages.sh
+
+echo "Reboot, after that. run the reinstall-packages.sh script located in your home directory"
+
 exit 0
